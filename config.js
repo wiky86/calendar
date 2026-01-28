812d4b15af6241df8ca6468a44f16f083315fcab
wiky86 <wiky86@naver.com>
2026-01-28 17:10:36 +0900

    Update Google API Key

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  * Since this is a client-side only app, they are exposed here.
  */
 const CONFIG = {
-    GOOGLE_API_KEY: 'AIzaSyD-zp3ID1MdWeMMQoSMTzHsGcvXZVnNe4k',
+    GOOGLE_API_KEY: 'AIzaSyDbDR6boR8GkJb0O3jo-lV_mhaq9ATb2TU',
     GOOGLE_CLIENT_ID: '1020447382727-aijr9luk2gtneo8cfpps23da41sqdfnl.apps.googleusercontent.com',
     GOOGLE_SHEET_ID: '1upfeAj22FEoYAgtSckJLQJ-Tg6FWFxU1ea3IdjfhGzM',
     SHEET_TAB_NAME: 'calendar'
